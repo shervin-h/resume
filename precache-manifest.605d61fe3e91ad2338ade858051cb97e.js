d5a8cf37b8cea0778b495d56d8af5f506231e0f6
Shervin Hasanzadeh <shervin.hz07@gmail.com>
2021-02-24 01:42:51 +0330

    Updates

---
+++ precache-manifest.605d61fe3e91ad2338ade858051cb97e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cea460119a9d5802de4",
+    "url": "/resume/static/css/main.a39fa273.chunk.css"
+  },
+  {
+    "revision": "6cea460119a9d5802de4",
+    "url": "/resume/static/js/main.6cea4601.chunk.js"
+  },
+  {
+    "revision": "6573bfd597e05fd615be",
+    "url": "/resume/static/js/1.6573bfd5.chunk.js"
+  },
+  {
+    "revision": "af7fdbda585bb1fd355c",
+    "url": "/resume/static/css/2.df2bdfa9.chunk.css"
+  },
+  {
+    "revision": "af7fdbda585bb1fd355c",
+    "url": "/resume/static/js/2.af7fdbda.chunk.js"
+  },
+  {
+    "revision": "29e3f0d89f61bc7eac17",
+    "url": "/resume/static/js/runtime~main.29e3f0d8.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/resume/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b0ef4539cf278ed159a649f0faae229b",
+    "url": "/resume/index.html"
+  }
+];
